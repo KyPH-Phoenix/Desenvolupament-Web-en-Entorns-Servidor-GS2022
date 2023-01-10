606cffbf415dac634bacb51a4b743350eb022965
Tu Nombre <you@example.com>
2023-01-10 20:35:35 +0100

    X2

---
+++ Intellij/Countries/src/main/java/com/liceu/countries/model/Country.java
@@ -0,0 +1,5 @@
+package com.liceu.countries.model;
+
+public class Country {
+
+}
---
+++ Intellij/Countries/src/main/java/com/liceu/countries/services/CountryService.java
@@ -0,0 +1,8 @@
+package com.liceu.countries.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountryService {
+
+}
